6e2ff5744d5b10236a116da5a9ea2d44cc55dece
jorgeortizc05 <jorgeortizc05@gmail.com>
2020-09-15 13:27:48 -0500

    Establecion parametro para control difuso humedad tiempo

--- src/main/java/jorgeortiz/smartnature/main/Ventana.java
+++ src/main/java/jorgeortiz/smartnature/main/Ventana.java
@@ -4,7 +4,7 @@ import net.sourceforge.jFuzzyLogic.FIS;
 
 public class Ventana extends javax.swing.JFrame{
 
-	private javax.swing.JSlider sldCalor;
+	private javax.swing.JSlider sldHumedad;
 	private net.sourceforge.jFuzzyLogic.FIS FIS;
 	private net.sourceforge.jFuzzyLogic.plot.JDialogFis jDialogFis;
 	private net.sourceforge.jFuzzyLogic.FunctionBlock functionBlock;
@@ -12,24 +12,24 @@ public class Ventana extends javax.swing.JFrame{
 	public Ventana(){
 		super("JFuzzy Logic");
 		
-		sldCalor=new javax.swing.JSlider();
-		sldCalor.setMinimum(0);
-		sldCalor.setMaximum(150);
-		sldCalor.setMajorTickSpacing(10);
-		sldCalor.setMinorTickSpacing(1);
-		sldCalor.setPaintTicks(true);
-		sldCalor.setPaintLabels(true);
+		sldHumedad=new javax.swing.JSlider();
+		sldHumedad.setMinimum(0);
+		sldHumedad.setMaximum(1023);
+		sldHumedad.setMajorTickSpacing(10);
+		sldHumedad.setMinorTickSpacing(1);
+		sldHumedad.setPaintTicks(true);
+		sldHumedad.setPaintLabels(true);
 		
-		sldCalor.addChangeListener(new javax.swing.event.ChangeListener(){
+		sldHumedad.addChangeListener(new javax.swing.event.ChangeListener(){
 			public void stateChanged(javax.swing.event.ChangeEvent c){
-				FIS.setVariable("calor",new Double(sldCalor.getValue()/10.0).doubleValue());
+				FIS.setVariable("humedad",new Double(sldHumedad.getValue()).doubleValue());
 				FIS.evaluate();
 				jDialogFis.repaint();
 				
 			}
 		});
 // 		setLayout(new java.awt.FlowLayout());
-		add(sldCalor);
+		add(sldHumedad);
 
 		setSize(700,300);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -40,14 +40,14 @@ public class Ventana extends javax.swing.JFrame{
 	private void cargarFCL(){
 		FIS=net.sourceforge.jFuzzyLogic.FIS.load("ControlDifuso.fcl",true);
 		jDialogFis=new net.sourceforge.jFuzzyLogic.plot.JDialogFis(FIS);
-		FIS.setVariable("calor",new Double(sldCalor.getValue()/10.0));
+		FIS.setVariable("humedad",new Double(sldHumedad.getValue()));
 		FIS.evaluate();
 		net.sourceforge.jFuzzyLogic.plot.JFuzzyChart.get().chart(FIS);
 		functionBlock=FIS.getFunctionBlock(null);
-		double v=functionBlock.getVariable("velocidad").getLatestDefuzzifiedValue();
+		double v=functionBlock.getVariable("riego").getLatestDefuzzifiedValue();
 		System.out.println("Valor: "+v);
 		
-		net.sourceforge.jFuzzyLogic.plot.JFuzzyChart.get().chart(functionBlock.getVariable("velocidad"), functionBlock.getVariable("velocidad").getDefuzzifier(), true);
+		net.sourceforge.jFuzzyLogic.plot.JFuzzyChart.get().chart(functionBlock.getVariable("riego"), functionBlock.getVariable("riego").getDefuzzifier(), true);
 	}
 	
 	public static void main(String [] args){
