8605068e1dfc29f22412d1f45b7d1ce900d1eff3
Jorge Ortiz <jorge@localhost.localdomain>
2020-11-11 12:04:10 -0500

    add parameters humedadSuelo, humedadAmbiente, temperaturaAmbiente

--- src/main/java/jorgeortiz/smartnature/main/Ventana.java
+++ src/main/java/jorgeortiz/smartnature/main/Ventana.java
@@ -5,6 +5,8 @@ import net.sourceforge.jFuzzyLogic.FIS;
 public class Ventana extends javax.swing.JFrame{
 
 	private javax.swing.JSlider sldHumedad;
+	private javax.swing.JSlider sldTempAmb;
+	private javax.swing.JSlider sldHumAmb;
 	private net.sourceforge.jFuzzyLogic.FIS FIS;
 	private net.sourceforge.jFuzzyLogic.plot.JDialogFis jDialogFis;
 	private net.sourceforge.jFuzzyLogic.FunctionBlock functionBlock;
@@ -20,16 +22,53 @@ public class Ventana extends javax.swing.JFrame{
 		sldHumedad.setPaintTicks(true);
 		sldHumedad.setPaintLabels(true);
 		
+		sldTempAmb=new javax.swing.JSlider();
+		sldTempAmb.setMinimum(0);
+		sldTempAmb.setMaximum(45);
+		sldTempAmb.setMajorTickSpacing(10);
+		sldTempAmb.setMinorTickSpacing(1);
+		sldTempAmb.setPaintTicks(true);
+		sldTempAmb.setPaintLabels(true);
+		
+		sldHumAmb=new javax.swing.JSlider();
+		sldHumAmb.setMinimum(0);
+		sldHumAmb.setMaximum(100);
+		sldHumAmb.setMajorTickSpacing(10);
+		sldHumAmb.setMinorTickSpacing(1);
+		sldHumAmb.setPaintTicks(true);
+		sldHumAmb.setPaintLabels(true);
+		
 		sldHumedad.addChangeListener(new javax.swing.event.ChangeListener(){
 			public void stateChanged(javax.swing.event.ChangeEvent c){
-				FIS.setVariable("humedad",new Double(sldHumedad.getValue()).doubleValue());
+				FIS.setVariable("humedadSuelo",new Double(sldHumedad.getValue()).doubleValue());
 				FIS.evaluate();
 				jDialogFis.repaint();
 				
 			}
 		});
+		
+		sldHumedad.addChangeListener(new javax.swing.event.ChangeListener(){
+			public void stateChanged(javax.swing.event.ChangeEvent c){
+				FIS.setVariable("temperaturaAmbiente",new Double(sldTempAmb.getValue()).doubleValue());
+				FIS.evaluate();
+				jDialogFis.repaint();
+				
+			}
+		});
+		
+		sldHumedad.addChangeListener(new javax.swing.event.ChangeListener(){
+			public void stateChanged(javax.swing.event.ChangeEvent c){
+				FIS.setVariable("humedadAmbiente",new Double(sldHumAmb.getValue()).doubleValue());
+				FIS.evaluate();
+				jDialogFis.repaint();
+				
+			}
+		});
+		
 // 		setLayout(new java.awt.FlowLayout());
 		add(sldHumedad);
+		add(sldTempAmb);
+		add(sldHumAmb);
 
 		setSize(700,300);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -40,7 +79,13 @@ public class Ventana extends javax.swing.JFrame{
 	private void cargarFCL(){
 		FIS=net.sourceforge.jFuzzyLogic.FIS.load("ControlDifuso.fcl",true);
 		jDialogFis=new net.sourceforge.jFuzzyLogic.plot.JDialogFis(FIS);
-		FIS.setVariable("humedad",new Double(sldHumedad.getValue()));
+		sldHumedad.setValue(2);
+		sldTempAmb.setValue(35);
+		sldHumAmb.setValue(29);
+		System.out.println(sldHumedad.getValue());
+		FIS.setVariable("humedadSuelo",new Double(sldHumedad.getValue()));
+		FIS.setVariable("temperaturaAmbiente",new Double(sldTempAmb.getValue()));
+		FIS.setVariable("humedadAmbiente",new Double(sldHumAmb.getValue()));
 		FIS.evaluate();
 		net.sourceforge.jFuzzyLogic.plot.JFuzzyChart.get().chart(FIS);
 		functionBlock=FIS.getFunctionBlock(null);
